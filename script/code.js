31b2ea8213d69d7e17b3164b31ae23b55f6070bb
Mardoqueiro <mordecai.dimbu@gmail.com>
2024-05-17 09:37:55 +0200

    Script.js

---
+++ script/code.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+let str = 'hola';
+    console.log(typeof str);
+    str = 5; 
+    console.log(typeof str);
+let num = 5;
+    console.log(typeof num);
+    num = 'hola'; 
+    console.log(typeof num);
+let bool = true;
+    console.log(typeof bool);
+    bool = 'hola';
+    console.log(typeof bool);
+
+let arr = [1, 2, 3, 4, 5];
+    console.log(typeof arr);
+    arr = 'hola';
+    console.log(typeof arr);
+let obj = {
+    name: 'Mardoqueiro ',
+    age: 20 
+};
+console.log(typeof obj);
+obj = 'Hola';
+console.log(typeof obj);
+let und = undefined; 
+    console.log(typeof und);
+    und = 'Hola';
+    console.log(typeof und);
+let nul = null;
+    console.log(typeof nul);
+    nul = 'Hola';
+    console.log(typeof nul);
+}
